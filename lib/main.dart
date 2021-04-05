81279d947c914225f7892f0772b9257c97b373ba
Franck W <franck.rj.w@gmail.com>
2021-04-05 19:55:17 +0200

    Ajout de bitsdojo_window pour changer le nom de la fenêtre.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 
 import 'hvlov_app.dart';
 
 void main() {
   runApp(HvlovApp());
+
+  doWhenWindowReady(() {
+    appWindow.title = "HVlov";
+    appWindow.show();
+  });
 }
